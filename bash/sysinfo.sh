d80fe6d41d8c361b1e74528f9c8c7d65b3f92ac0
SukhrajAmrit <200495973@student.georgianc.on.ca>
2023-02-01 22:45:07 -0500

    my new script

---
+++ bash/sysinfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "The domain name is:"
+hostname
+ech0 "The host information is:"
+hostnamectl
+echo "The IP address of server is:"
+hostname -I
+echo "Available space in the root system:"
+df -h /
